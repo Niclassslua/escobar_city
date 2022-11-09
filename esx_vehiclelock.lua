8f4a84d56b6cca8890635df1dd2b2b62a6b7162a
Niclassslua <78554432+Niclassslua@users.noreply.github.com>
2022-11-09 17:19:28 +0100

    made vehicle lock heli, plane and boat compatible

--- esx_vehiclelock.lua
+++ esx_vehiclelock.lua
@@ -3,3 +3,4 @@ Finished vehicle lock logic
 Added correct notify for lockage
 fixed vehicle trunk access while locked
 removed unnecessary vehicle lock conrols
+made vehicle lock heli, plane and boat compatible
